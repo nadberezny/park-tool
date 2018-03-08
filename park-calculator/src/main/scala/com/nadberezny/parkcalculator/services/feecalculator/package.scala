92c6b622434c26fecabe9d1950c6674d1d326fa0
Juliusz Nadberezny <nadberezny@gmail.com>
2018-03-08 10:48:15 +0100

    Park Calc: Add package object for fee calculator namespace.

--- park-calculator/src/main/scala/com/nadberezny/parkcalculator/services/feecalculator/package.scala
+++ park-calculator/src/main/scala/com/nadberezny/parkcalculator/services/feecalculator/package.scala
@@ -14,4 +14,41 @@ package object feecalculator {
   )
 
   case class Factor(durationInterval: Long, value: Double)
+
+  case class FactorRow(
+    rank: Int, duration: Long, regular: Double, vip: Double
+  )
+
+  object FactorRepository {
+    def all: Option[List[FactorRow]] = {
+      Some(List(
+        FactorRow(0, 60, 1, 0),
+        FactorRow(1, 60, 2, 2),
+        FactorRow(2, 60, 4, 3)
+      ))
+    }
+  }
+
+  case class PriceRow(currency: String, amount: Double)
+
+  object PriceRepository {
+    def find(currency: String): Option[PriceRow] = {
+      currency match {
+        case "USD" => Some(PriceRow(currency, 1.0))
+        case _ => None
+      }
+    }
+  }
+
+  case class VehicleRow(vehicleId: String, isVip: Boolean)
+
+  object VehicleRepository {
+    def find(vehicleId: String): Option[VehicleRow] = {
+      vehicleId match {
+        case "" => None
+        case "RSTNA01" => Some(VehicleRow(vehicleId, isVip = true))
+        case _ => Some(VehicleRow(vehicleId, isVip = false))
+      }
+    }
+  }
 }
