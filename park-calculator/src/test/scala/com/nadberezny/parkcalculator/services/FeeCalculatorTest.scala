0e9bfb974e4943dd4be50797119eecc7f90918ba
Juliusz Nadberezny <nadberezny@gmail.com>
2018-03-08 10:54:09 +0100

    Park Calc: Add FeeCalculator.

---
+++ park-calculator/src/test/scala/com/nadberezny/parkcalculator/services/FeeCalculatorTest.scala
@@ -0,0 +1,27 @@
+package com.nadberezny.parkcalculator.services
+
+import com.nadberezny.parkcalculator.testutils.TestParkingRepo
+import org.joda.time.DateTime
+import org.scalatest.FunSuite
+
+class FeeCalculatorTest extends FunSuite {
+  implicit val now = DateTime.now
+  implicit val testRepo = new TestParkingRepo(duration = 30)
+  lazy val calculator = new FeeCalculator()
+
+
+
+  test("happy path") {
+    val result = calculator.calculate("RST", "USD")
+
+    assert(result.isRight)
+    assert(result.map(_.fee) == Right(1.0))
+  }
+
+  test("failure") {
+    val expectedFailure = calculator.calculate("RST", currency = "XXX")
+    assert(expectedFailure.isLeft)
+    assert(expectedFailure == Left("Price info unavailable."))
+
+  }
+}
